7dad804ee571421cc63b7f26f0e8bec9ac9750f2
DHADHAYA <145863092+DHADHAYA@users.noreply.github.com>
2025-11-17 11:21:40 +0200

    Added an API

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function handleSearchSubmit(event){
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#weather-app-city");
 
-	event.preventDefault();
-	let searchInput = document.querySelector("#search-form-input");
-	let cityElement = document.querySelector("#city");
-	cityElement.innerHTML = searchInput.value;
+  let temperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+  cityElement.innerHTML = response.data.name;
 }
 
+function searchCity(city) {
+  let apiKey = "d4d46bbf69e026814e12986d4ca2fa70"; 
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  
+  axios.get(apiUrl).then(refreshWeather);
+}
 
-let searchFormElement = document.querySelector("search-form");
-searchFormElement.addEventListener("submit",handleSearchSubmit);
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Lisbon");
+
